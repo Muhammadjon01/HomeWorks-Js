acf62b985cb628ebb4671f46d3679b39e71923ad
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-05-17 16:10:55 +0500

    Add Hometask-Function

---
+++ HomeWorks-Object/HomeWork1.js
@@ -0,0 +1,14 @@
+let University ={
+    name: "TNU",
+    addres:"Dushanbe",
+    faculties:{
+        nameFaculty:"Mex-Mat",
+        course:{
+            courseName:"Information",
+        }
+    }
+}
+
+let {faculties} =University
+let {course} = faculties
+console.log(course)
